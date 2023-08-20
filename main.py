5efc8bcda8bf8d09c0dd4dca0be3c152d82c9938
jacob <jweeksdev@gmail.com>
2023-08-20 16:59:38 -0400

    Cleaner logging to vector database

--- main.py
+++ main.py
@@ -22,29 +22,26 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 active_threads = []
 MODEL = os.getenv('MODEL')
 
-
-def accumulate_and_ingest_messages(messages, last_ingested_index, username):
-    print(last_ingested_index)
-    messages = [msg for index, msg in enumerate(messages) if index > last_ingested_index and index != 1]
+def accumulate_and_ingest_messages(messages, username):
+    messages = [msg for index, msg in enumerate(messages) if index > st.session_state["last_ingested_index"] - 1 and index != 1 and index != 2]
     if messages:
         ingest_user_chat(username, messages, datetime.now().isoformat())  # Assuming username and timestamp are parameters
+        st.session_state["last_ingested_index"] = len(st.session_state.messages)
     return messages
 
-last_update_time = datetime.now()
 def update_func():
     # Clear the list of active threads
     active_threads.clear()
     if "messages" in st.session_state:  # Check if messages exist
         update_messages = st.session_state.messages.copy()
-        accumulate_and_ingest_messages(st.session_state.messages, st.session_state["last_ingested_index"], unique_username)  # unique_username assumed to be user's username
-        st.session_state["last_ingested_index"] = len(update_messages) - 1  # Update the last ingested index
+        accumulate_and_ingest_messages(st.session_state.messages, unique_username)  # unique_username assumed to be user's username
 
         # Getting the current timestamp with full details
         timestamp = datetime.now().strftime("%A, %B %d, %Y %H:%M:%S %p %Z") # Example: Sunday, August 19, 2023 17:30:15 PM UTC
         
         # Send the timestamp into update_prompt
-        system_content = update_prompt(timestamp)
-        update_messages.append({"role": "user", "content": system_content})
+        content = update_prompt(timestamp)
+        update_messages.append({"role": "user", "content": content})
         
         response = openai.ChatCompletion.create(model=MODEL, messages=update_messages)
         msg = response.choices[0].message
@@ -77,7 +74,7 @@ else:
     
     # Initialize a state to track the last ingested message index
     if "last_ingested_index" not in st.session_state:
-        st.session_state["last_ingested_index"] = -1
+        st.session_state["last_ingested_index"] = 2
 
     for index, msg in enumerate(st.session_state.messages):
         if msg["role"] != "system" and index > 1:  # Skip system messages
@@ -90,8 +87,7 @@ else:
         msg = response.choices[0].message
         st.session_state.messages.append(msg)
         st.chat_message("assistant").write(msg.content)
-
-        accumulate_and_ingest_messages(st.session_state.messages, st.session_state["last_ingested_index"], unique_username)  # unique_username assumed to be user's username
+        accumulate_and_ingest_messages(st.session_state.messages, unique_username)  # unique_username assumed to be user's username
         t = threading.Thread(target=delay_notify)
         add_script_run_ctx(t)
         active_threads.append(t) # Add the new thread to the list
--- prompt_templates/update_prompt.py
+++ prompt_templates/update_prompt.py
@@ -1,2 +1,2 @@
 def update_prompt(timestamp):
-    return f""""{timestamp}: Update instructions"""
+    return f""""{timestamp}: Continue to probe me the user for more information. Try to get me to set a schedule, or make a plan, or tell you more about my intentions."""
